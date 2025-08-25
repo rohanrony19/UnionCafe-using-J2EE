3344850a42550f595b74ac96480ec527f59e253f
Rohan <rohanprasad1434@gmail.com>
2025-08-25 23:29:12 +0530

    Cafe`

---
+++ UnionCafe/src/main/java/com/xworkz/Bakery/servlet/ProfileServlet.java
@@ -0,0 +1,52 @@
+package com.xworkz.Bakery.servlet;
+
+import com.xworkz.Bakery.dto.RegisterDTO;
+import com.xworkz.Bakery.service.RegisterService;
+import com.xworkz.Bakery.service.RegisterServiceImpli;
+//import sun.plugin.dom.core.Element;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/Profile")
+public class ProfileServlet extends HttpServlet {
+    private RegisterService registerService = new RegisterServiceImpli();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("in profile");
+        HttpSession httpSession=req.getSession(false);
+        if (httpSession == null || httpSession.getAttribute("mail") == null) {
+            resp.sendRedirect("Login.jsp");
+            return;
+        }
+
+        String email = (String) httpSession.getAttribute("mail");
+
+        // Call service to fetch details from DB
+
+        RegisterDTO dto = registerService.displayDetailFromEmail(email);
+
+        if (dto == null) {
+            // Email not found in DB → force login again
+            resp.sendRedirect("Login.jsp");
+            return;
+        }
+
+        // Set attributes for JSP
+        req.setAttribute("fullName", dto.getFullName());
+        req.setAttribute("phoneNumber", dto.getPhoneNumber());
+        req.setAttribute("email", dto.getEmail());
+
+        RequestDispatcher rd = req.getRequestDispatcher("Profile.jsp");
+        rd.forward(req, resp);
+    }
+}
+
+
