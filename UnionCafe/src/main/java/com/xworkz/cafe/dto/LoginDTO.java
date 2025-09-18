d6591d03901ea3feae808e1ab78443ec63b6cc5a
Rohan <rohanprasad1434@gmail.com>
2025-09-18 22:00:41 +0530

    Cafe`

--- UnionCafe/src/main/java/com/xworkz/cafe/dto/LoginDTO.java
+++ UnionCafe/src/main/java/com/xworkz/cafe/dto/LoginDTO.java
@@ -1,15 +1,25 @@
 package com.xworkz.cafe.dto;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
-@Getter
-@EqualsAndHashCode
+@Data
 public class LoginDTO implements Serializable {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is invalid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+            message = "Password must be at least 8 characters with letters, numbers and special characters"
+    )
     private String password;
+
 }
