488b77c6500017e259f695dc4056ab6f77ce47d4
Rohan <rohanprasad1434@gmail.com>
2025-09-18 22:01:21 +0530

    Cafe`

--- UnionCafe/src/main/java/com/xworkz/cafe/service/RegisterService.java
+++ UnionCafe/src/main/java/com/xworkz/cafe/service/RegisterService.java
@@ -1,10 +1,15 @@
 package com.xworkz.cafe.service;
 
 import com.xworkz.cafe.dto.RegisterDTO;
+import jakarta.mail.MessagingException;
 
 public interface RegisterService {
     String validate(RegisterDTO registerDTO);
     String loginValidation(String mail, String password);
-    String forgotPassword(String email,String password,String confirmPassword);
+//    String forgotPassword(String email,String password,String confirmPassword);
     RegisterDTO displayDetailFromEmail( String Email);
+    boolean checkMail(String email);
+    void sendOtpEmail(String email, int otp) throws MessagingException;
+    public boolean isEmailRegistered(String email);
+    boolean updatePassword(String email, String newPassword);
 }
