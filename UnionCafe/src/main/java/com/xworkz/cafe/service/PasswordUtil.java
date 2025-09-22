9efb1380f420f790664bb017389ee88f39015cd4
Rohan <rohanprasad1434@gmail.com>
2025-09-22 23:12:41 +0530

    Cafe`

---
+++ UnionCafe/src/main/java/com/xworkz/cafe/service/PasswordUtil.java
@@ -0,0 +1,10 @@
+package com.xworkz.cafe.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+
+    public static String hashPassword(String plainPassword) {
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt(12));
+    }
+}
