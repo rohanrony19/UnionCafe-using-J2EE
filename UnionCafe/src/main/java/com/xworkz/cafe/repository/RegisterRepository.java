505447aa68bd78c970920e0187f033361541e78d
Rohan <rohanprasad1434@gmail.com>
2025-09-23 22:28:19 +0530

    Cafe`

--- UnionCafe/src/main/java/com/xworkz/cafe/repository/RegisterRepository.java
+++ UnionCafe/src/main/java/com/xworkz/cafe/repository/RegisterRepository.java
@@ -1,12 +1,17 @@
 package com.xworkz.cafe.repository;
 
 import com.xworkz.cafe.dto.RegisterDTO;
+import com.xworkz.cafe.entity.RegisterEntity;
 
 public interface RegisterRepository {
-    void save(RegisterDTO dto);
-    boolean checkMail(String mail);
-    String getPassword(String mail);
-    boolean updatePassword(String mail, String password);
-    RegisterDTO displayAllDataFromEmail(String email );
-    public void saveResetToken(String email, String token);
+    boolean save(RegisterEntity entity);
+    boolean checkMail(String email);
+    RegisterEntity findByEmail(String email);
+    boolean updatePassword(String email,String password);
+//    RegisterEntity findByEmail(String email);
+//    boolean checkMail(String mail);
+//    String getPassword(String mail);
+//    boolean updatePassword(String mail, String password);
+//    RegisterDTO displayAllDataFromEmail(String email );
+//    public void saveResetToken(String email, String token);
 }
