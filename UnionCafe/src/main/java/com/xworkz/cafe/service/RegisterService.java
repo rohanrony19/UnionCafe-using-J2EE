eb9fe630a979e91540825892c4ae494f77ba2e8b
Rohan <rohanprasad1434@gmail.com>
2025-09-24 21:58:56 +0530

    Cafe`

--- UnionCafe/src/main/java/com/xworkz/cafe/service/RegisterService.java
+++ UnionCafe/src/main/java/com/xworkz/cafe/service/RegisterService.java
@@ -4,12 +4,19 @@ import com.xworkz.cafe.dto.RegisterDTO;
 import jakarta.mail.MessagingException;
 
 public interface RegisterService {
-    String validate(RegisterDTO registerDTO);
-    String loginValidation(String mail, String password);
-//    String forgotPassword(String email,String password,String confirmPassword);
-    RegisterDTO displayDetailFromEmail( String Email);
-    boolean checkMail(String email);
-    void sendOtpEmail(String email, int otp) throws MessagingException;
-    public boolean isEmailRegistered(String email);
+    boolean save(RegisterDTO registerDTO);
+    boolean login(String email, String password);
+    boolean isEmailRegistered(String email);
+    void sendOtpEmail(String email, int otpValue) throws MessagingException;
     boolean updatePassword(String email, String newPassword);
+    RegisterDTO displayDetailFromEmail(String email);
+    boolean existsByEmail(String email);
+//    String validate(RegisterDTO registerDTO);
+//    String loginValidation(String mail, String password);
+////    String forgotPassword(String email,String password,String confirmPassword);
+//    RegisterDTO displayDetailFromEmail( String Email);
+//    boolean checkMail(String email);
+//    void sendOtpEmail(String email, int otp) throws MessagingException;
+//    public boolean isEmailRegistered(String email);
+//    boolean updatePassword(String email, String newPassword);
 }
