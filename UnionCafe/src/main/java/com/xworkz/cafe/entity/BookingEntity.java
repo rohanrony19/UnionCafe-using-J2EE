cc8d0cdc07af1c519bd19fd7efc3418641f8bfd9
Rohan <rohanprasad1434@gmail.com>
2025-09-28 22:13:52 +0530

    Cafe`

---
+++ UnionCafe/src/main/java/com/xworkz/cafe/entity/BookingEntity.java
@@ -0,0 +1,36 @@
+package com.xworkz.cafe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "bookings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "check_in_date")
+    private LocalDate checkInDate;   // SQL DATE
+
+    @Column(name = "check_in_time")
+    private LocalTime checkInTime;   // SQL TIME
+
+    @Column(name = "number_of_guests")
+    private int numberOfGuests;
+}
