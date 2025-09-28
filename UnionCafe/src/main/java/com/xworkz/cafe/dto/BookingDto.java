7964c1009466f702246afb71036f64b68a61d2c7
Rohan <rohanprasad1434@gmail.com>
2025-09-28 22:13:45 +0530

    Cafe`

---
+++ UnionCafe/src/main/java/com/xworkz/cafe/dto/BookingDto.java
@@ -0,0 +1,39 @@
+package com.xworkz.cafe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Invalid email")
+    private String email;
+
+    @Pattern(regexp = "\\d{10}", message = "Phone number must be 10 digits")
+    private String phoneNumber;
+
+    @FutureOrPresent(message = "Check-in date must be today or future")
+    private LocalDate checkInDate;
+
+    private LocalTime checkInTime;
+
+    @Min(value = 1, message = "At least 1 guest required")
+    private int numberOfGuests;
+
+
+}
